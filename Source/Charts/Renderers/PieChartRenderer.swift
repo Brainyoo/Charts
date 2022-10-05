be7a45a97b3a08a9463bb15e309d2c8c96a3e582
Christoph Schacht <christoph.schacht@lyth.de>
2022-10-05 08:41:51 +0200

    fixed merge error

--- Source/Charts/Renderers/PieChartRenderer.swift
+++ Source/Charts/Renderers/PieChartRenderer.swift
@@ -150,8 +150,6 @@ open class PieChartRenderer: NSObject, DataRenderer
         let singularPrefix: String = chart.data?.accessibilityEntryLabelSingularPrefix ?? "Element"
         let pluralPrefix: String = chart.data?.accessibilityEntryLabelPluralPrefix ?? "Elements"
 
-        let description = chart.chartDescription.text ?? dataSet.label ?? chart.centerText ??  "Pie Chart"
-
         let description = chart.chartDescription.text ?? chart.centerText ?? dataSet.label ??  "Pie Chart"
 
         let
